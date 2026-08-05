b36c0dc920176e9536c3ffbb4771e9a348debdfa
Anuj Jain <ajain39861@gmail.com>
2026-08-05 23:15:21 +0530

    Time: 18 ms (63.21%), Space: 108.1 MB (32%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> intset = new HashSet<>();
+
+        for(int num : nums){
+            if(intset.contains(num)){
+                return true;
+            }   
+            intset.add(num);
+        }
+    return false;
+    }
+}
